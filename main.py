08706afae5afbc90d5aafad09d3411ffe123acae
Erik <51793348+ErikFaustmann@users.noreply.github.com>
2020-10-19 22:14:44 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,35 +1,23 @@
 import os
 from os import path
 from matplotlib import pyplot as plt
+import numpy as np
 import argparse
 import logging
-#import torch
-#import torchvision.models as models
-#from torch import nn
+import time
 
-from functions.model import create_model
-from functions.cmc import compute_cmc
+from functions.functions import create_directories, create_model, compute_cmc, calculate_features
 from functions.normcorr import calc_corr
 
-folder = 'C:\\Users\\User\\virtualtest\\MCNCC2\\datasets\\FID-300'
-
-# Logger
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-
-formatter = logging.Formatter('%(levelname)s:%(message)s')
-
-file_handler = logging.FileHandler('mcncc.log')
-file_handler.setFormatter(formatter)
-
-logger.addHandler(file_handler)
-
 # Argparser
-parser = argparse.ArgumentParser(description='take some indiviudal folders from user')
+parser = argparse.ArgumentParser(description='take some individual folders from user')
+parser.add_argument('-f', '--folder', type=str, help='define folder containing the dataset')
 parser.add_argument('-t', '--tracks', type=str, default='tracks_cropped_Subset', help='define track folder')
 parser.add_argument('-rf', '--refs', type=str, default='Subset', help='define reference folder')
 parser.add_argument('-str', '--stride', type=int, default='1', help='stride for convolutions')
-parser.add_argument('-avgp', '--avgpool_bool', default='False', help='activate average pooling for features')
+parser.add_argument('-avgp', '--avgpool_bool', default='False', action='store_true', help='activate average pooling for features')
+parser.add_argument('-avgp_str', '--avgp_stride', type=int, default='1', help='stride for average_pooling')
+parser.add_argument('-skf', '--skip_feat', default=False, action='store_true', help='skip feature generation')
 parser.add_argument('-r', '--rot', default=False, action='store_true', help='add rotation')
 parser.add_argument('-ris', '--start', type=int, default=-10, help='rotation interval start')
 parser.add_argument('-rie', '--end', type=int, default=11, help='rotation interval end')
@@ -40,36 +28,78 @@ parser.add_argument('-lbltable', '--label_file', type=str, default='Subsetlabels
 
 args = parser.parse_args()
 
+
 # Paths
+folder = args.folder
 tracks = path.join(folder, args.tracks)
 refs = path.join(folder, args.refs)
 
 ref_l = [f for f in os.listdir(refs) if f.endswith('.png')]
 track_l = [f for f in os.listdir(tracks) if f.endswith('.jpg')]
 
+
+# Creating additional directories
+create_directories(folder)
+
+
+# Logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(message)s')
+
+file_handler = logging.FileHandler(path.join(folder,'logs/mcncc.log'))
+file_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+
+
+
+
 if __name__ == "__main__":
-    logger.info('refs: {}'.format(args.refs))
-    logger.info('refs: {}'.format(args.tracks))
-    logger.info('refs: {}'.format(args.stride))
-    logger.info('refs: {}'.format(args.rot))
-    logger.info('refs: {}'.format(args.start))
-    logger.info('refs: {}'.format(args.end))
-    logger.info('refs: {}'.format(args.cmc))
-    logger.info('refs: {}'.format(args.scorefile))
-    logger.info('refs: {}'.format(args.cmc_file))
-    logger.info('refs: {}'.format(args.label_file))
-    logger.info('refs: {}'.format(args.avgpool_bool))
+    logger.info('folder: {}'.format(args.folder))
+    logger.info('reference_folder: {}'.format(args.refs))
+    logger.info('track_folder: {}'.format(args.tracks))
+    logger.info('stride: {}'.format(args.stride))
+    logger.info('skip_feat: {}'.format(args.skip_feat))
+    logger.info('rotation: {}'.format(args.rot))
+    logger.info('start: {}'.format(args.start))
+    logger.info('end: {}'.format(args.end))
+    logger.info('cmc: {}'.format(args.cmc))
+    logger.info('scorefile: {}'.format(args.scorefile))
+    logger.info('cmc_file: {}'.format(args.cmc_file))
+    logger.info('label_file: {}'.format(args.label_file))
+    logger.info('avg_bool: {}'.format(args.avgpool_bool))
+    logger.info('avg_bool_stride: {}'.format(args.avgp_stride))
 
 # Model
-device, model = create_model(args.avgpool_bool)
+device, model = create_model(args.avgpool_bool, args.avgp_stride)
+
+# Feature Calculation
+calculate_features(model, device, folder, track_l, tracks, ref_l, refs, args.skip_feat, args.start, args.end)
+
+# Feature paths
+feature_tracks = folder + '/features/tracks'
+feature_refs = folder + '/features/refs'
+feature_refs_rot = folder + '/features/refs_rot'
+
+feature_ref_l = [f for f in os.listdir(feature_refs) if f.endswith('.npy')]
+feature_ref_rot_l = [f for f in os.listdir(feature_refs_rot) if f.endswith('.npy')]
+feature_track_l = [f for f in os.listdir(feature_tracks) if f.endswith('.npy')]
 
 # Calculation of the score matrix
-score_mat = calc_corr(model, track_l, ref_l, tracks, refs, device, args.stride, args.rot, args.start, args.end, args.scorefile)
+calc_time = time.time()
+score_mat = calc_corr(feature_track_l, feature_ref_l, feature_ref_rot_l, feature_tracks, feature_refs, feature_refs_rot, device, args.stride, args.rot, args.start, args.end, args.scorefile)
+elapsed = time.time() - calc_time
 
+logger.info("elapsed time: {}".format(elapsed))
+logger.info("score_mat saved")
 
 # CMC-score
 if args.cmc:
-    cmc_score = compute_cmc(score_mat, folder, args.label_file)
+
+    score_mat = np.load(args.scorefile)
+    cmc_score = compute_cmc(score_mat, args.label_file)
 
     f, ax = plt.subplots(1)
     plt.plot(cmc_score)
@@ -77,4 +107,4 @@ if args.cmc:
     plt.ylabel('cmc-score')
     ax.set_ylim(bottom=0)
     plt.grid(True)
-    f.savefig(cmc_score_diagram)
+    f.savefig('cmc_figure')
